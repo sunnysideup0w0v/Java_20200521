36a049c3c40213617d4cff79948e76cab71f0438
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-21 11:34:42 +0900

    intent 설정

--- app/src/main/java/com/example/java_20200521/MainActivity.java
+++ app/src/main/java/com/example/java_20200521/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.databinding.DataBindingComponent;
 import androidx.databinding.DataBindingUtil;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.java_20200521.databinding.ActivityMainBinding;
 
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
         setContentView(R.layout.activity_main);
         setupEvents();
         setValues();
@@ -24,7 +26,18 @@ public class MainActivity extends AppCompatActivity {
 
     // 이벤트 처리 코드 모아두는곳
     void setupEvents(){
+        binding.goToFirstBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //FirstActivity로 이동
+                //여행에 비유
 
+                //티켓발권
+                Intent myIntent = new Intent(MainActivity.this, FirstActivity.class);
+                //실제출발
+                startActivity(myIntent);
+            }
+        });
     }
 
     // 화면에 나와야할 데이터 세팅 코드 모아두는곳
