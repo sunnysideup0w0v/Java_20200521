7c3942e0f4545c9e347dc405905df6f675e0b558
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-21 11:20:55 +0900

    data binding

--- app/src/main/java/com/example/java_20200521/MainActivity.java
+++ app/src/main/java/com/example/java_20200521/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.java_20200521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingComponent;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.java_20200521.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         setContentView(R.layout.activity_main);
         setupEvents();
         setValues();
